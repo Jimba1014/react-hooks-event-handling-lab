689e9bff0fd369d8b94250bba3cbe022761f53a7
Jim <jimba.ed@outlook.com>
2022-09-21 10:14:24 -0400

    Made Changes for Class

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+
+function addFocus(){
+    console.log("Good!")
+}
+function addBlur(){
+    console.log("Hey! Eyes on me!")
+}
+
+
+function EyesOnMe(){
+    return (
+        <button onClick={addFocus}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,18 @@
 // Code Keypad Component Here
 
+function handleChange(event){
+    console.log("Entering password...")
+
+}
+
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <form>
+                <input type="password" onChange={handleChange}/>
+            </form>
+            
+        </div>
     )
 }
 
